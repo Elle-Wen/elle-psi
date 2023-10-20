28e25127f9bc9ec07b19ef3727fbc67852ea7eb0
Granoia <nicholasfranzese2026@u.northwestern.edu>
2023-10-20 10:37:13 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Getting Started
+
+- the `test` directory should hold source code for files you want to run
+- if you want to make a new file, go into `test/CMakeLists.txt` and append `add_test_case_with_run( <name of file> )`
+- the `bin` directory will hold executables
+- use the command `cmake . && make` from the project directory to compile
+- after that, try these two commands: `./bin/test_millionaires 1 12345 20 & ./bin/test_millionaires 2 12345 15` and `./bin/test_millionaires 1 12345 75 & ./bin/test_millionaires 2 12345 34`
+- go check out `test/millionaires.cpp` and see if you can understand it. Try some other inputs as well.
---
+++ test/millionaires.cpp
@@ -0,0 +1,61 @@
+#include <emp-sh2pc/emp-sh2pc.h>
+
+using namespace emp;
+using namespace std;
+
+void test_millionaire(int number){
+	// emp::Integer is a class that implements a secure integer for MPC
+	// the constructor takes 3 arguments: <number of bits>, <value>, <party>
+	Integer a(32, number, ALICE); // an integer initialized with party ALICE is only visible to party 1
+	Integer b(32, number, BOB);   // similarly, if initialized with party BOB it is only visible to party 2
+	// (ALICE and BOB are macros for 1 and 2 in the emp namespace)
+	
+	// emp::Bit is a class that implements a secure bit for MPC
+	Bit res = a > b; // res is the output of securely comparing a and b in the circuit
+	// the > operator on two emp::Integers invokes a circuit for comparison from the emp library
+	
+	// the reveal() method outputs a secured value
+	cout << "ALICE has larger number?\t" << res.reveal<bool>() << endl;
+	// res.reveal<bool>() lets the compiler know that we'd like to unmask the Bit res, which contains the result of the comparison, and that we'd like its type to be bool
+}
+
+void test_sum(int number) {
+	// see if you can pattern match to write a circuit that sums two Integers from alice and bob, and then reveals the sum
+	// note that the + operator is defined on emp::Integers
+	// YOUR CODE HERE
+}
+
+
+int main(int argc, char** argv) {
+	if (argc <= 2) {
+		cout << "Please supply proper arguments:" << endl;
+		cout << "./<millionaires executable> <party> <port> <number>" << endl;
+		return 0;
+	}
+
+	int port, party;
+	parse_party_and_port(argv, &party, &port); // this is a function from emp
+	// it just makes it so that when you run the following:
+	// ./bin/millionaires 1 12345
+	// it interprets the 1 as the party, and the 12345 as the port
+
+	int num = 20; // default value for num
+	if (argc > 3) {
+		num = atoi(argv[3]);
+	}
+
+	// initialize emp::NetIO class, handles communication between the two parties
+	NetIO * io = new NetIO(party==ALICE ? nullptr : "127.0.0.1", port);
+
+	setup_semi_honest(io, party); // initialize circuit
+
+	test_millionaire(num);
+
+	//test_sum(num);
+
+//	cout << CircuitExecution::circ_exec->num_and()<<endl; // gives the number of AND gates in the circuit
+
+	finalize_semi_honest();
+	delete io;
+	return 0;
+}
