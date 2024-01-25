94989fc7e1fabeb4138b4661a36cca882dafb372
Yuheng Wen <yuhengellewen@gmail.com>
2024-01-25 10:24:11 -0800

    2d point for fuzzyPSI

--- test/fuzzyPSI.cpp
+++ test/fuzzyPSI.cpp
@@ -1,4 +1,3 @@
-// given 2d points, compute the fuzzy PSI for 2 party computation // use mahanton distance -> use square 
 #include <emp-sh2pc/emp-sh2pc.h>
 #include <iostream>
 #include <list>
@@ -6,7 +5,7 @@
 #include <algorithm>
 using namespace emp;
 using namespace std;
-void fuzzy(vector<int> &elements, Integer &distance, int party){ // 1d, naive PSI 
+void fuzzy_1d(vector<int> &elements, Integer &distance, int party){ // 1d, naive PSI 
     vector<Bit> intersection; 
     for (size_t i = 0; i < elements.size(); i++){
         for (size_t j = 0; j < elements.size(); j++){
@@ -27,19 +26,41 @@ void fuzzy(vector<int> &elements, Integer &distance, int party){ // 1d, naive PS
     }
 }
 
+void fuzzy_2d(vector<vector<int>> &elements, Integer &distance, int party){
+    vector<Bit> intersection;
+    for (size_t i = 0; i < elements.size(); i++){
+        for (size_t j = 0; j < elements.size(); j++){
+            Integer a(32, elements[i][0], ALICE); // assume same size for A and B
+            Integer b(32, elements[j][0], BOB);
+            Integer diff_x = a - b;
+            Integer abs_diff_x = diff_x.abs();
+            Integer c(32, elements[i][1], ALICE); // assume same size for A and B
+            Integer d(32, elements[j][1], BOB);
+            Integer diff_y = c - d;
+            Integer abs_diff_y = diff_y.abs();
+            Integer sum = abs_diff_x + abs_diff_y;
+            Bit inside = (distance >= sum); 
+            cout << "i,j: " << i << ", " << j << "\t=\t" << inside.reveal<bool>() << endl;
+            intersection.push_back(inside);
+        }
+    }
+    
+}
+
 int main(int argc, char** argv) {
-        //expect argument: [party] [port] [epsilon] [elements-x] 
-        // epsilon-ball 
+        //expect argument: [party] [port] [distance] [elements-x] [elements-y]
+        // manhattan distance 
         int port, party;
 	    parse_party_and_port(argv, &party, &port);  
         NetIO * io = new NetIO(party==ALICE ? nullptr : "127.0.0.1", port);
         setup_semi_honest(io, party); // initialize circuit
         int64_t input_distance = atoi(argv[3]);
         Integer distance(32, input_distance, PUBLIC); 
-        vector<int> elements;
-        for (int i = 4; i < argc; ++i) {
-            elements.push_back(atoi(argv[i]));
+        vector<vector<int>> elements;
+        for (int i = 4; i < argc; i+=2) {
+            vector<int> coordinate = {atoi(argv[i]), atoi(argv[i+1])};
+            elements.push_back(coordinate);
         } 
-        fuzzy(elements, distance, party);
+        fuzzy_2d(elements, distance, party);
         finalize_semi_honest();
 } 
