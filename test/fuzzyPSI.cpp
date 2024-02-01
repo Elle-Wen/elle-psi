cfa5d647d05a7ace497685bab7fcc4eb121ad39d
Yuheng Wen <yuhengellewen@gmail.com>
2024-02-01 08:49:04 -0800

    3d fuzzy

--- test/fuzzyPSI.cpp
+++ test/fuzzyPSI.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 using namespace emp;
 using namespace std;
+
 void fuzzy_1d(vector<int> &elements, Integer &distance, int party){ // 1d, naive PSI 
     vector<Bit> intersection; 
     for (size_t i = 0; i < elements.size(); i++){
@@ -44,11 +45,34 @@ void fuzzy_2d(vector<vector<int>> &elements, Integer &distance, int party){
             intersection.push_back(inside);
         }
     }
-    
 }
 
+void fuzzy_3d (vector<vector<int>> &elements, Integer &distance, int party){
+    vector<Bit> intersection;
+    for (size_t i = 0; i < elements.size(); i++){
+        for (size_t j = 0; j < elements.size(); j++){
+            Integer a(32, elements[i][0], ALICE); // assume same size for A and B
+            Integer b(32, elements[j][0], BOB);
+            Integer diff_x = a - b;
+            Integer abs_diff_x = diff_x.abs();
+            Integer c(32, elements[i][1], ALICE); // assume same size for A and B
+            Integer d(32, elements[j][1], BOB);
+            Integer diff_y = c - d;
+            Integer abs_diff_y = diff_y.abs();
+            Integer e(32, elements[i][2], ALICE); // assume same size for A and B
+            Integer f(32, elements[j][2], BOB);
+            Integer diff_z = e - f;
+            Integer abs_diff_z = diff_z.abs();
+            Integer sum = abs_diff_x + abs_diff_y + abs_diff_z;
+            Bit inside = (distance >= sum); 
+            cout << "i,j: " << i << ", " << j << "\t=\t" << inside.reveal<bool>() << endl;
+            intersection.push_back(inside);
+        }
+    }
+    
+}
 int main(int argc, char** argv) {
-        //expect argument: [party] [port] [distance] [elements-x] [elements-y]
+        //expect argument: [party] [port] [distance] [elements-x] [elements-y] [elements-z]
         // manhattan distance 
         int port, party;
 	    parse_party_and_port(argv, &party, &port);  
@@ -57,10 +81,10 @@ int main(int argc, char** argv) {
         int64_t input_distance = atoi(argv[3]);
         Integer distance(32, input_distance, PUBLIC); 
         vector<vector<int>> elements;
-        for (int i = 4; i < argc; i+=2) {
-            vector<int> coordinate = {atoi(argv[i]), atoi(argv[i+1])};
+        for (int i = 4; i < argc; i+=3) {
+            vector<int> coordinate = {atoi(argv[i]), atoi(argv[i+1]), atoi(argv[i+2])};
             elements.push_back(coordinate);
         } 
-        fuzzy_2d(elements, distance, party);
+        fuzzy_3d(elements, distance, party);
         finalize_semi_honest();
 } 
